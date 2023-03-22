e5afa228eb45cbba2fcf9feed3d17b7a11c31ad6
al3jo10 <alejandro.burbano01@uceva.edu.co>
2023-03-22 09:38:44 -0500

    se conecto con la base de datos y se le agrego algunos paises a la base de datos

---
+++ src/main/resources/import.sql
@@ -0,0 +1,6 @@
+INSERT INTO  pais (nombre) VALUES ('colombia');
+INSERT INTO  pais (nombre) VALUES ('peru');
+INSERT INTO  pais (nombre) VALUES ('venezuela');
+INSERT INTO  pais (nombre) VALUES ('ecuador');
+INSERT INTO  pais (nombre) VALUES ('bolivia');
+INSERT INTO  pais (nombre) VALUES ('argentina');
